f2aee6b3ec614d92bdd6252dfcd38f855535a10f
Amin Ahmed Khan <aa2kba@gmail.com>
2025-10-01 16:29:12 +0400

    refactor: Remove obsolete test scripts for CrewAI and RAG services

--- server/test.py
+++
@@ -1,7 +0,0 @@
-from phoenix.client import Client
-
-
-phoenix_client = Client()
-prompt_name = "agent_role"
-prompt = phoenix_client.prompts.get(prompt_identifier=prompt_name)
-print(prompt.format().messages[0].get("content").strip())
